5b6d6965c84ad6c89b197f48e82ee21fd2952268
Justin Rudio <justinjrudio@gmail.com>
2016-08-26 11:32:07 -0700

    added a couple of commands to the tool:
    new
      - config
    
    test

--- cmd/otp-tool/main.go
+++ cmd/otp-tool/main.go
@@ -1,21 +1,21 @@
+// Copyright © 2016 Justin Rudio justinjrudio@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"gopkg.in/urfave/cli.v1"
-)
+import "github.com/jrudio/otp-reloaded/cmd/otp-tool/cmd"
 
 func main() {
-	app := cli.NewApp()
-
-	app.Name = "otp"
-	app.Usage = "Automate tasks related to your Plex Media Server"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("You have ran otp!")
-		return nil
-	}
-
-	app.Run(os.Args)
+	cmd.Execute()
 }
