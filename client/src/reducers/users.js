532599a07d4d32aba2e376395329cc49cf4739f0
Justin Rudio <justinjrudio@gmail.com>
2018-03-08 15:47:34 -0800

    fixed adding users to store

--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -7,18 +7,18 @@ import {
 export default (state = {}, action) => {
     switch (action.type) {
         case ADD_USER:
-            console.log(action)
-
-            delete action.type
-            let newUser = action
-
-            return Object.assign({}, state, {
-                list: [].push(state.list, newUser)
-            })
+        console.log(action)
+        
+        delete action.type
+        let newUser = action
+        
+        return Object.assign({}, state, {
+            list: [].push(state.list, newUser)
+        })
         case MONITORED_USERS_FETCH:
-            return Object.assign({}, state, {
-                isLoading: true
-            })
+        return Object.assign({}, state, {
+            isLoading: true
+        })
         case MONITORED_USERS_HAVE_BEEN_FETCHED:
             let { users } = action
 
